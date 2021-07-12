a543cb751ddf9337dcc18e318f96e40853fae938
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-07-12 18:50:24 +0100

    add pop function, add select answerfunction

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-const gameOptions = (() => {
+const game = (() => {
   //cache DOm
   const optionBtns = document.querySelectorAll(".options");
   const modal = document.querySelector(".modal");
+  const qBox = document.querySelector(".questionBox");
+  const qNumDisplay = document.querySelector(".qNum");
+  const timerDisplay = document.querySelector(".timer");
+  const answerBubbles = document.querySelectorAll(".bubble");
 
   //add event listeners
   optionBtns.forEach((btn) => btn.addEventListener("click", whichGame));
-
+  answerBubbles.forEach((bubble) =>
+    bubble.addEventListener("click", selectAnswer)
+  );
   //variables
   let gameOption;
+  let qNum;
+  let answer;
 
   function whichGame(e) {
     if (e.target.innerText == "Addition") {
@@ -23,6 +31,18 @@ const gameOptions = (() => {
     console.log(gameOption);
 
     modal.classList.toggle("hidden");
+  }
+
+  function selectAnswer(e) {
+    answer = e.target.innerText;
+    console.log(answer);
+    pop(e);
+  }
+
+  function pop(e) {
+    let bubbleNum = e.target.classList[1];
+    console.log(bubbleNum);
+    e.target.remove();
   }
 })();
 
