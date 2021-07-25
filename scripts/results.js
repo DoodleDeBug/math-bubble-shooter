3ffd5843c1d911f9c84783b5be985f100d38e048
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-07-25 13:19:11 +0100

    make style changes

--- scripts/results.js
+++ scripts/results.js
@@ -24,6 +24,7 @@ const results = (() => {
     q++;
 
     const fullQuestion = document.createElement("span");
+    fullQuestion.classList.add("fullQ");
     fullQuestion.innerText = `${question}`;
     div.appendChild(fullQuestion);
 
